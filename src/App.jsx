593074841a7029318f6debc229066522cb869d88
Umar Ejaz <umar.senpaiiii@gmail.com>
2025-02-21 09:34:02 -0800

    2nd animation

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const App = () => {
   return (
     <motion.div className="w-full h-screen flex justify-center items-center bg-zinc-800">
-      <motion.div
+      {/* <motion.div
         // initial={{ x: -1000, rotate: 0 }}
         animate={{
           x: 100,
@@ -12,6 +12,9 @@ const App = () => {
         }}
         transition={{
           duration: 2,
+          repeat: Infinity,
+          repeatType: "reverse",
+          ease:"anticipate"
           // type: "spring",
           // stiffness: 100,
           // damping: 20,
@@ -19,7 +22,27 @@ const App = () => {
         className="w-40 h-40 flex justify-center items-center bg-black text-white text-xl"
       >
         <motion.h1 className="h1">h1 ! my name is umar</motion.h1>
-      </motion.div>
+      </motion.div> */}
+        <motion.div 
+        animate={{
+          x: [0,200,200,0,0],
+          y: [0,0,200,200,0],
+          // rotate: 180,
+        }}
+        transition={{
+          duration: 2,
+          repeat: Infinity,
+          
+          ease:"anticipate"
+          // type: "spring",
+          // stiffness: 100,
+          // damping: 20,
+        }}
+        className="w-20 h-20 bg-red-500"
+
+        >
+          hiii
+        </motion.div>
     </motion.div>
   );
 };

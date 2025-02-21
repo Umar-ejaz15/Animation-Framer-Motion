d2fc127420e07e796c96b195dd5f3867b0956491
Umar Ejaz <umar.senpaiiii@gmail.com>
2025-02-21 11:07:57 -0800

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,121 @@
-import { motion } from "motion/react";
+import { motion, useScroll, useTransform } from "framer-motion";
 import React from "react";
 
 const App = () => {
+  // Scroll-based animations for opacity and vertical position
+  const { scrollYProgress, scrollY } = useScroll();
+  const opacity = useTransform(scrollYProgress, [0, 1], [0, 1]); // Scroll effect on opacity
+  const y = useTransform(scrollYProgress, [0, 1], [50, 0]); // Scroll effect on vertical position
+
+  // Introduce a delay effect (simulating stagger) based on scroll position
+  const delay = useTransform(scrollYProgress, [0, 0.5, 1], [0.2, 0.1, 0]); // Delay simulation for stagger
+
+  // Add a scrub effect to the scroll position
+  const scrub = useTransform(scrollY, [0, 1000], [0, 1]);
+
   return (
-    <motion.div className="w-full h-screen flex justify-center items-center bg-zinc-800">
-      {/* <motion.div
-        // initial={{ x: -1000, rotate: 0 }}
-        animate={{
-          x: 100,
-          rotate: 180,
-        }}
-        transition={{
-          duration: 2,
-          repeat: Infinity,
-          repeatType: "reverse",
-          ease:"anticipate"
-          // type: "spring",
-          // stiffness: 100,
-          // damping: 20,
-        }}
-        className="w-40 h-40 flex justify-center items-center bg-black text-white text-xl"
-      >
-        <motion.h1 className="h1">h1 ! my name is umar</motion.h1>
-      </motion.div> */}
+    <>
       <motion.div
-        // animate={{
-        //   x: [0, 200, 200, 0, 0],
-        //   y: [0, 0, 200, 200, 0],
-        //   // rotate: 180,
-        // }}
-        whileHover={{
-          scale: 1.5,
-          borderRadius: "100%",
+        style={{
+          scaleX: scrollYProgress,
+          width: "100%",
         }}
-        whileTap={{
-          scale: 0.8,
-          borderRadius: "0%",
-        }}
-        drag
-        dragConstraints={{
-          top: -100,
-          left: -100,
-          right: 100,
-          bottom: 100,
-        }}
-        dragDirectionLock="true"
-        // transition={{
-        //   duration: 2,
-        //   repeat: Infinity,
-        //   ease: "anticipate",
-        //   // type: "spring",
-        //   // stiffness: 100,
-        //   // damping: 20,
-        // }}
-        className="w-20 h-20 bg-red-500 flex justify-center items-center"
-      >
-        hiii
-      </motion.div>
-    </motion.div>
+        className="h-2 origin-left bg-red-500 fixed top-0"
+      ></motion.div>
+
+      <div className="w-full h-auto font-mono px-20 flex gap-20 items-center flex-col bg-white text-zinc-950 dark:bg-zinc-950 dark:text-white">
+        <motion.h1
+          style={{
+            opacity,
+            y,
+            transitionDelay: delay, // Apply stagger delay effect
+          }}
+          className="text-3xl mb-10 mt-20"
+        >
+          Welcome to my developer portfolio
+        </motion.h1>
+
+        <motion.p
+          style={{
+            opacity: scrub,
+            y: useTransform(scrub, [0, 1], [100, 0]), // Scrubbing effect on scroll for paragraph
+          }}
+          className="text-justify"
+        >
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim sapiente
+          praesentium pariatur facilis quos alias! Nobis in sapiente esse? Nulla
+          accusantium modi vel expedita quas reiciendis libero dicta aut dolore
+          culpa, incidunt voluptatem odio hic repudiandae itaque minus beatae
+          sapiente quaerat! Rem molestiae, eos nam et tempora tempore amet
+          dolores ratione provident quam in, praesentium eveniet voluptatem
+          repellendus commodi facilis nesciunt! Ullam molestias aliquid
+          obcaecati cupiditate nobis voluptatum porro eligendi pariatur, odit
+          laborum maxime vel.
+        </motion.p>
+
+        <hr className="bg-white w-full h-[1px] mt-5 mb-5" />
+
+        <motion.p
+          style={{
+            opacity: scrub,
+            y: useTransform(scrub, [0, 1], [100, 0]), // Scrubbing effect on scroll for paragraph
+          }}
+          className="text-justify"
+        >
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim sapiente
+          praesentium pariatur facilis quos alias! Nobis in sapiente esse? Nulla
+          accusantium modi vel expedita quas reiciendis libero dicta aut dolore
+          culpa, incidunt voluptatem odio hic repudiandae itaque minus beatae
+          sapiente quaerat! Rem molestiae, eos nam et tempora tempore amet
+          dolores ratione provident quam in, praesentium eveniet voluptatem
+          repellendus commodi facilis nesciunt! Ullam molestias aliquid
+          obcaecati cupiditate nobis voluptatum porro eligendi pariatur, odit
+          laborum maxime vel.
+        </motion.p>
+
+        <hr className="bg-white w-full h-[1px] mt-5 mb-5" />
+
+        <motion.p
+          style={{
+            opacity: scrub,
+            y: useTransform(scrub, [0, 1], [100, 0]), // Scrubbing effect on scroll for paragraph
+          }}
+          className="text-justify"
+        >
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim sapiente
+          praesentium pariatur facilis quos alias! Nobis in sapiente esse? Nulla
+          accusantium modi vel expedita quas reiciendis libero dicta aut dolore
+          culpa, incidunt voluptatem odio hic repudiandae itaque minus beatae
+          sapiente quaerat! Rem molestiae, eos nam et tempora tempore amet
+          dolores ratione provident quam in, praesentium eveniet voluptatem
+          repellendus commodi facilis nesciunt! Ullam molestias aliquid
+          obcaecati cupiditate nobis voluptatum porro eligendi pariatur, odit
+          laborum maxime vel.
+        </motion.p>
+
+        <hr className="bg-white w-full h-[1px] mt-5 mb-5" />
+        
+        <motion.p
+          style={{
+            opacity: scrub,
+            y: useTransform(scrub, [0, 1], [100, 0]), // Scrubbing effect on scroll for paragraph
+          }}
+          className="text-justify"
+        >
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim sapiente
+          praesentium pariatur facilis quos alias! Nobis in sapiente esse? Nulla
+          accusantium modi vel expedita quas reiciendis libero dicta aut dolore
+          culpa, incidunt voluptatem odio hic repudiandae itaque minus beatae
+          sapiente quaerat! Rem molestiae, eos nam et tempora tempore amet
+          dolores ratione provident quam in, praesentium eveniet voluptatem
+          repellendus commodi facilis nesciunt! Ullam molestias aliquid
+          obcaecati cupiditate nobis voluptatum porro eligendi pariatur, odit
+          laborum maxime vel.
+        </motion.p>
+
+        <hr className="bg-white w-full h-[1px] mt-5 mb-5" />
+      </div>
+    </>
   );
 };
 
